e1b6af376a8f602438bb26ae3f130478afbadf25
Egor Shchukin <vikzelikov@yandex.ru>
2023-04-15 12:07:21 +0300

    add fibonacci and stats handlers

---
+++ main_basic.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go Profiling Demo Server\n")
+}
